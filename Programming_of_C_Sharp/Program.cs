ce62e89aa4e09ffc10933c9395cd622786687b6f
sameer8953 <sameer.ahmadid8953@gmail.com>
2024-08-13 17:15:04 +0530

    Math logic

--- Programming_of_C_Sharp/Program.cs
+++ Programming_of_C_Sharp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ public class BooleanExample
+ {
+     public static void Main(string[] args)
+    {
+        //bool isValid = true;
+        //sbyte level = 23;
+       // System.Console.WriteLine(level);
+
+       int firstNumber, Diduc, secondNumber;
+			// Assigning a constant to variable
+			firstNumber = 10;
+            Diduc = 5;
+			Console.WriteLine("First Number = {0}", firstNumber);
+
+			// Assigning a variable to another variable
+			secondNumber = Diduc + firstNumber;
+			Console.WriteLine("Second Number = {0}", secondNumber);
+            Console.WriteLine("Diduc = {0}",Diduc);
+    }
+ }
